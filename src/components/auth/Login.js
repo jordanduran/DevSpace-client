d19faba9a5186c4bb9e279b4efe5e5a08b91e06e
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2019-11-01 21:55:57 -0400

    Endpoint change

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -22,7 +22,7 @@ const Login = () => {
       <input type="submit" className="btn btn-primary" value="Login" />
     </form>
     <p className="my-1">
-      Don't have an account? <a href="register.html">Sign Up</a>
+      Don't have an account? <a href="/register">Sign Up</a>
     </p></div>
 };
 
