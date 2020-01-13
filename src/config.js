2b85be826a36b9e8f3cb69499613fecdf3e55c1d
Raman Aujla <ramanpreet.aujla@gmail.com>
2020-01-12 19:34:16 -0500

    created a variable to user endpoints

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
+const path = `http://localhost:8000`;
+// const path = `https://stormy-crag-28024.herokuapp.com`;
 export default {
-    API_ENDPOINT: `https://stormy-crag-28024.herokuapp.com`,
+    API_ENDPOINT: path,
     API_KEY: process.env.REACT_APP_API_KEY
 }
