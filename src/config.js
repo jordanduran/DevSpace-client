53ab69f7c44c50ceb9eb3c40401a186c2f8fd869
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2020-01-12 19:52:34 -0500

    fixed path

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-const path = `http://localhost:8000`;
-// const path = `https://stormy-crag-28024.herokuapp.com`;
+// const path = `http://localhost:8000`;
+const path = `https://stormy-crag-28024.herokuapp.com`;
 export default {
-    API_ENDPOINT: path,
-    API_KEY: process.env.REACT_APP_API_KEY
-}
+  API_ENDPOINT: path,
+  API_KEY: process.env.REACT_APP_API_KEY
+};
